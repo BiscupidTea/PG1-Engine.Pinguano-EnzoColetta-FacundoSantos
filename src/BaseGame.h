4874c010e46225d73a376833419996031b7dea3c
fekox <facusantos123@gmail.com>
2023-08-15 12:32:36 -0300

    Add the documentation on the BaseEngine class

--- src/BaseGame.h
+++ src/BaseGame.h
@@ -7,7 +7,10 @@ using namespace window;
 using namespace renderer;
 
 namespace baseEngine
-{
+{	
+	/// <summary>
+	/// Class BaseGame
+	/// </summary>
 	class BaseGame
 	{
 		private:
@@ -17,8 +20,18 @@ namespace baseEngine
 
 		public:
 
+		/// <summary>
+		/// Initialize the engine.
+		/// </summary>
+		/// <returns>If the engine is on or off</returns>
 		int initEngine();
+		/// <summary>
+		/// Core loop of the engine.
+		/// </summary>
 		void updateEngine();
+		/// <summary>
+		/// Close engine.
+		/// </summary>
 		void closeEngine();
 	};
 }
