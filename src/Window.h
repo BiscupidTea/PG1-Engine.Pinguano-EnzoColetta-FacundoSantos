85e237e295900c00c77f84da20240a4c026fbb24
fekox <facusantos123@gmail.com>
2023-08-15 11:01:49 -0300

    Add documentation in the window class.

--- src/Window.h
+++ src/Window.h
@@ -3,6 +3,9 @@
 
 namespace window
 {
+	/// <summary>
+	/// Class window.
+	/// </summary>
 	class Window
 	{
 	private:
@@ -11,9 +14,32 @@ namespace window
 
 	public:
 
+		/// <summary>
+		/// Initialize the window.
+		/// </summary>
+		/// <param name="width">Screen width</param>
+		/// <param name="height">Screen height</param>
+		/// <param name="windowName">Window name</param>
+		/// <param name="monitor">to change the full screen mode</param>
+		/// <param name="share">to share resources</param>
 		void initWindow(int width, int height, const char* windowName, GLFWmonitor* monitor, GLFWwindow* share);
+		
+		/// <summary>
+		/// Calls glfwTerminate() funtion to destroy all remaining windows and cursors, reset any modified resources.
+		/// </summary>
 		void closeWindow();
+
+		/// <summary>
+		/// Calls GLFWwindowShouldClose, to check if window close value is true or false. 
+		/// </summary>
+		/// <param name="window"></param>
+		/// <returns></returns>
 		bool windowShouldClose(GLFWwindow* window);
+
+		/// <summary>
+		/// Get window.
+		/// </summary>
+		/// <returns></returns>
 		GLFWwindow* getWindow();
 
 	};
