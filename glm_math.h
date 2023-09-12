305ebc5deb9942a191e6cf2b0fdc4e71a895de8a
fekox <facusantos123@gmail.com>
2023-09-12 12:13:14 -0300

    Include gml_math in the engine

---
+++ glm_math.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <glm.hpp>
+
+#include <vec2.hpp>
+#include <vec3.hpp>
+#include <vec4.hpp>
+#include <mat3x2.hpp>
+#include <mat3x3.hpp>
+#include <mat4x4.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
