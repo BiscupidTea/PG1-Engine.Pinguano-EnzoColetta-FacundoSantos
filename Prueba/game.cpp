145d50dde95528c709c3a6278c5d4505283fba54
fekox <facusantos123@gmail.com>
2023-09-26 14:25:44 -0300

    Fix the scale and create Aimy

--- Prueba/game.cpp
+++ Prueba/game.cpp
@@ -1,4 +1,7 @@
 #include "game.h"
+#include <iostream>
+
+using namespace std;
 
 Game::Game(float width, float height, const char* windowName) : BaseGame(width, height, windowName)
 {
@@ -15,20 +18,33 @@ Game::~Game()
 
 void Game::init()
 {
-	Vector4 colorTriangle = Vector4{ 1.0f, 0.4f, 0.4f, 1 };
-	Vector3 position = Vector3{ width/2,height/2,0 };
-	Vector3 scale = Vector3{ 200,200,1 };
-	Vector3 rotation = Vector3{ 0,0,0 };
-	triangle = new Shape(Shape::typeShapes::Square, colorTriangle, GetRenderer(), position, scale, rotation);
-	triangle->setRotationZ(90);
+	colorTriangle = Vector4{ 1.0f, 0.5f, 0.0f, 1 };
+	Vector4 colorTriangle2 = Vector4{ 1.0f, 0.0f, 0.5f, 1 };
+	position = Vector3{ width / 2, height,0 };
+	scale = Vector3{ 1,1,1 };
+	rotation = Vector3{ 0,0,0 };
+
+	Vector3	scale2 = Vector3{ 50,50,50 };
+
+	Vector3 position2 = Vector3{ width - scale2.x, height /2,0 };
+
+	triangle = new Shape(Shape::typeShapes::Triangle, colorTriangle, GetRenderer(), position, scale, rotation);
+	Aimy = new Shape(Shape::typeShapes::Triangle, colorTriangle2, GetRenderer(), position2, scale2, rotation);
 }
 
 void Game::update()
 {
+	triangle->setPosition(Vector3{ triangle->getPosition().x, triangle->getPosition().y - 1.0f,0 });
+	Vector3 scale = triangle->getScale();
+
+	triangle->setScale({triangle->getScale().x + 1.f , triangle->getScale().y + 1.f ,1.f });
+
 	triangle->Draw();
+	Aimy->Draw();
 }
 
 void Game::exit()
 {
 	delete triangle;
+	delete Aimy;
 }
--- Prueba/game.h
+++ Prueba/game.h
@@ -8,11 +8,16 @@ class Game : public BaseGame
 {
 private:
 	Shape* triangle;
+	Shape* Aimy;
 	const char* windowName;
 
 public:
 	float width;
 	float height;
+	Vector4 colorTriangle;
+	Vector3 position;
+	Vector3 scale;
+	Vector3 rotation;
 
 	Game(float width, float height, const char* windowName);
 	~Game();
