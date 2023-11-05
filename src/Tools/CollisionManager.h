a411b00f3b11788b316a49d53f416a2a1f7e274b
fekox <facusantos123@gmail.com>
2023-11-05 20:04:32 -0300

    The Collision Manager class was ordered

--- src/Tools/CollisionManager.h
+++ src/Tools/CollisionManager.h
@@ -1,6 +1,14 @@
 #pragma once
+#include "Entity/Entity2D.h"
 
-class CollisionManager
+using namespace Entity2D;
+
+namespace collisionManager
 {
-};
+	class EXPORT CollisionManager : public Entity2D
+	{
+		public:
 
+			bool CheckCollisionBoxBox(Entity2D& entityOne, Entity2D& entityTwo);
+	};
+}
